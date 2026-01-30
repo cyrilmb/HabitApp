837c9893fc162944759339ad1838ed409f6eb95e
Cyril Malle-Barlow <cyril.mallebarlow@gmail.com>
2026-01-30 09:16:30 -0600

    Initial Commit

---
+++ HabitApp/HabitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitAppApp.swift
+//  HabitApp
+//
+//  Created by Cyril Malle-Barlow on 1/30/26.
+//
+
+import SwiftUI
+
+@main
+struct HabitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
